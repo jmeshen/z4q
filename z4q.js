5d260e2ce3586589111a0a9b799161bec999523d
jmeshen <jmeshen@gmail.com>
2014-12-28 21:26:11 -0500

    media queries and refactor

--- z4q.js
+++ z4q.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-
-
-
-  //var audioElement = document.createElement('audio');
-  //audioElement.setAttribute('src', 'z4q-cl.mp3');
-
-  //audioElement.load()
-  /*$.get();
-  audioElement.addEventListener("load", function() {
-    audioElement.play();
-  }, true);*/
-
   var audio = new Audio('z4q-cl.mp3');
-
   $('.btn').click(function() {
     audio.play();
     $("#1").delay(600).fadeIn();
@@ -22,5 +9,4 @@ $(document).ready(function(){
     $("#5").delay(11600).fadeIn();
     $("#batman").delay(14000).fadeIn();
   });
-
 });
